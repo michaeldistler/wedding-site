086cb915e4af0caf88eeba5a5a110d73a32a16bf
michael <michael@michael-530U3C-530U4C-532U3C.(none)>
2015-11-25 15:52:02 -0500

    adjusting settings, removed .pyc files

--- wedding_app/settings.py
+++ wedding_app/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'user_donations',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -93,3 +92,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
