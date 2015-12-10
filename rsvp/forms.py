ffe586acfc50a2cf3e8efe053363507738e02fb2
michael <michael.distler@gmail.com>
2015-12-10 17:23:31 -0500

    Form Styled

--- rsvp/forms.py
+++ rsvp/forms.py
@@ -11,3 +11,29 @@ class RsvpForm(forms.ModelForm):
             "email",
             "number_of_guests",
         )
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'name': 'first_name',
+                'type': 'text',
+                'placeholder': 'First Name',
+                'class': 'form-control',
+            }),
+            'last_name': forms.TextInput(attrs={
+                'name': 'last_name',
+                'type': 'text',
+                'placeholder': 'Last Name',
+                'class': 'form-control',
+            }),
+            'email': forms.EmailInput(attrs={
+                'name': 'email',
+                'type': 'text',
+                'placeholder': 'Email',
+                'class': 'form-control',
+            }),
+            'number_of_guests': forms.NumberInput(attrs={
+                'name': 'number_of_guests',
+                'type': 'text',
+                'placeholder': 'Numero Of Guests',
+                'class': 'form-control',
+            }),
+        }
