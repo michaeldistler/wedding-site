d23d3f81d07ae6bfa528d21023344a375585b44d
michael <michael.distler@gmail.com>
2015-12-10 19:38:50 -0500

    Updated Procfile

--- wedding_app/settings.py
+++ wedding_app/settings.py
@@ -96,3 +96,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_HOST_USER = 'michael.distler@gmail.com'
+EMAIL_HOST_PASSWORD = 'dbmvheeynmbpdcfa'
+
