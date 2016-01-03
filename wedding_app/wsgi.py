975e1533e0d74bc1759c0e5cabee80bd434e1e9d
michael <michael.distler@gmail.com>
2016-01-03 18:22:48 -0500

    modified wsgi file

--- wedding_app/wsgi.py
+++ wedding_app/wsgi.py
@@ -12,6 +12,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE",
                       "wedding_app.settings.settings")
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
