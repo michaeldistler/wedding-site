8477c489b37451c7e0442a7f359f005cb639ab42
michael <michael.distler@gmail.com>
2016-01-03 17:14:01 -0500

    Modifying urls

--- wedding_app/urls.py
+++ wedding_app/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 from django.http import HttpResponse
 from django.contrib import admin
 
@@ -8,4 +9,10 @@ urlpatterns = patterns('',
     url(r'^RSVP/$', include('rsvp.urls', namespace="rsvp")),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^robots.txt$', lambda r: HttpResponse("User-agent: *\nDisallow: /", content_type="text/plain")),
+)
+
+urlpatterns += patterns('',
+    url(r'^static/(?P<path>*)$', 'django.views.static.serve', {
+        'document_root': settings.STATIC_ROOT
+    }),
 )
