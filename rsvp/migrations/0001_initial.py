406fe27c1a62c61cd35ec914c45f7e9cb5456973
michael <michael.distler@gmail.com>
2016-01-04 23:36:20 -0500

    adding new migrations

---
+++ rsvp/migrations/0001_initial.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Rsvp',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('first_name', models.CharField(max_length=100, null=True)),
+                ('last_name', models.CharField(max_length=100, null=True)),
+                ('email', models.EmailField(max_length=75, null=True)),
+                ('number_of_guests', models.IntegerField(null=True, blank=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
