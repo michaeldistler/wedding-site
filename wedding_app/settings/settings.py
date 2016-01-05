c34c062c355bf9adabcc6cdb6cabfdb66fd5f217
michael <michael.distler@gmail.com>
2016-01-04 23:53:14 -0500

    settings change

--- wedding_app/settings/settings.py
+++ wedding_app/settings/settings.py
@@ -2,7 +2,7 @@
 import dj_database_url
 import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
