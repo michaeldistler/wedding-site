dc8967bfc1a5968125b000693512da01d26656c6
michael <michael.distler@gmail.com>
2016-01-04 23:12:25 -0500

    updated settings

--- wedding_app/settings/settings.py
+++ wedding_app/settings/settings.py
@@ -84,6 +84,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "staticfiles"),
 )
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
